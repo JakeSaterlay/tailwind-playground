267937260a20e5487efc621ed0953968bfc2a2c6
JakeSaterlay <jakesaterlay@hotmail.co.uk>
2025-04-27 19:20:35 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+function App() {
+  return (
+    <div className="grid lg:grid-cols-3 gap-4 p-4">
+      <div className="bg-blue-500 h-24 rounded-lg flex items-center justify-center text-white">
+        1
+      </div>
+      <div className="bg-green-500 h-24 rounded-lg flex items-center justify-center text-white">
+        2
+      </div>
+      <div className="bg-red-500 h-24 rounded-lg flex items-center justify-center text-white">
+        3
+      </div>
+      <div className="bg-yellow-500 h-24 rounded-lg flex items-center justify-center text-white">
+        4
+      </div>
+      <div className="bg-purple-500 h-24 rounded-lg flex items-center justify-center text-white">
+        5
+      </div>
+      <div className="bg-pink-500 h-24 rounded-lg flex items-center justify-center text-white">
+        6
+      </div>
+    </div>
+  );
+}
+
+export default App;
